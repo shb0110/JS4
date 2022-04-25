8042eabd898727365d3c579354a700d81b240f31
shb0110 <99003926+shb0110@users.noreply.github.com>
2022-04-25 10:14:01 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,29 @@
+const kun= prompt(`Bugungi kunni kiriting`)
+if(kun<8||0>=kun){
+    alert(`bunday kun yoq`)
+}
+if(kun==1){
+alert(`Dushanba`)
+}
+if(kun==2){
+    alert(`Seshanba`)
+    }
+if(kun==3){
+alert(`Chorshanba`)
+    }
+if(kun==4){
+        alert(`Payshanba`)
+        }
+if(kun==5){
+    alert(`Juma`)
+     }
+    if(kun==6){
+      alert(`Shanba`)
+       }
+    if(kun==7){
+    alert(`Yakshanba`)
+        }
+                                             
+    console.log(kun)
+    
+    
